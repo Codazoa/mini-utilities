d3128bc5c4a44d0761c0989f0b00d79191f002e5
codazoa <codazoa@protonmail.com>
2023-01-27 11:00:02 -0800

    Basic functionality for cat.
    May need argument checking

--- my-cat.c
+++ my-cat.c
@@ -1,5 +1,25 @@
-// includes
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]){
+    // get a filepointer for reading
+    FILE *file_ptr;
+    file_ptr = fopen(argv[1], "r");
+
+    // check if the file was opened
+    if(file_ptr == NULL){
+        printf("Error opening file");
+        exit(1); 
+    }
+
+    // allocate a buffer for reading from file
+    char buf[256];
+    while(fgets(buf, sizeof(buf), file_ptr) != NULL){
+        // keep reading and printing from the fill until EOF
+        printf("%s", buf);
+    }
+
+    // print final newline character
+    printf("\n");
     return 0;
 }
