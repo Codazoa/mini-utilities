88c6bc6d63789617cb11053e1110aa34dfafd8a3
codazoa <codazoa@protonmail.com>
2023-02-08 20:07:15 -0800

    commenting

--- my-uniq.c
+++ my-uniq.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[]){
         FILE *file_ptr; 
         file_ptr = fopen(argv[i], "r");
 
-        // get a line
+        // initialize line variables
         char *line1 = NULL;
         char *line2 = NULL;
         size_t len1 = 0;
@@ -17,24 +17,31 @@ int main(int argc, char const *argv[]){
         size_t read1;
         size_t read2;
 
+        // read 2 lines at a time
         while ((read1 = getline(&line1, &len1, file_ptr)) != -1){
             read2 = getline(&line2, &len2, file_ptr);
             if (!line1 || !line2){
                 // memory allocation failed
+                if(line1) free(line1);
+                if(line2) free(line2);
                 fclose(file_ptr);
                 perror("Memory allocation failed");
                 exit(1);
             }
             
+            // reached the end of the file
             if (read2 == -1) {
                 printf("Here: %s", line1);
                 break;
             }
 
-            if (strcmp(line1, line2) != 0){ // lines are different
+            // lines are different
+            if (strcmp(line1, line2) != 0){ 
+                // print the line and seek back to grab next line
                 printf("%s", line1);
                 fseek(file_ptr, -read2, SEEK_CUR);
-            } else {
+            } else { // lines are same
+                // print the line and skip the next line
                 printf("%s", line1);
             }
             
@@ -44,79 +51,6 @@ int main(int argc, char const *argv[]){
 
         fclose(file_ptr);
     }
-
-
-
-    
-    // for (int i = 1; i < argc; i++){         //Go through each argument, which all should be files
-
-    //     printf("Initialize Variables\n");   
-    //     FILE *file_ptr;                     //Initialize variable to hold file
-    //     char* line = "";                  //Initialize variable to hold read line
-    //     char* lastline = "";              //Initialize variable to hold last line
-    //     size_t length = 0;                  //Initialize variable to hold the length
-    //     int ignore = 0;                     //Flag to only print once
-        
-    //     printf("Try and open file\n");
-    //     file_ptr = fopen(argv[i], "r");  //Attempt to open file given by user
-
-    //     printf("Check to see if file opened\n");
-    //     // check if the file was opened
-    //     if(file_ptr == NULL){               //If the file didn't open, error out and exit
-    //         perror("my-uniq: cannot open file\n");
-    //         exit(1); 
-    //     }
-
-
-    //     //Go through each line
-    //     printf("start while loop\n");
-    //     while ((getline(&line, &length, file_ptr)) != EOF){
-
-    //         printf("Check to see if we allocated memory\n");
-    //         if (!line){
-    //             // memory allocation failed
-    //             fclose(file_ptr);
-    //             perror("Memory allocation failed\n");
-    //             exit(1);
-    //         }
-            
-    //         printf("check to see if we arent the same as the last one\n");
-    //         //Reset out ignore flag if the last one didn't match
-    //         if (strcmp(line, lastline) != 0 ){
-    //             ignore = 0;
-    //         }
-
-    //         printf("Check to see if we should print\n");
-    //         //Check to see if we matched the last line and we haven't printed already
-    //         if ((strcmp(line, lastline ) == 0) && (ignore == 0)){
-    //             ignore = 1;             //Set our flag so we don't print again
-    //             printf("%s", line);     //print
-    //         }
-
-    //         //Copy the current line into last line for the next loop
-    //         strcpy(lastline, line);
-
-    //     }
-
-
-    // }
-    // printf("End\n");
-
-
-    
-    //iterate through argv with argc as count
-        //for each one, go through the file and detect repeated lines
-            //Store line
-            
-            //if line != lastline
-                //ignore = 0
-            
-            //if line == lastline and ignore==0
-                //ignore = 1
-                //print(line)
-            
-            
-            //lastline = line
 
     return 0;
 }
