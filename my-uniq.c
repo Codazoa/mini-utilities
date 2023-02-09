62006310fd96ca2992e394055e977092775fe63d
codazoa <codazoa@protonmail.com>
2023-02-08 18:53:57 -0800

    sorta working version

--- my-uniq.c
+++ my-uniq.c
@@ -5,60 +5,98 @@
 
 int main(int argc, char const *argv[]){
     printf("main\n");
-    
-    for (int i = 1; i < argc; i++){         //Go through each argument, which all should be files
 
-        printf("Initialize Variables\n");   
-        FILE *file_ptr;                     //Initialize variable to hold file
-        char* line = "";                  //Initialize variable to hold read line
-        char* lastline = "";              //Initialize variable to hold last line
-        size_t length = 0;                  //Initialize variable to hold the length
-        int ignore = 0;                     //Flag to only print once
-        
-        printf("Try and open file\n");
-        file_ptr = fopen(argv[i], "r");  //Attempt to open file given by user
+    for (int i = 1; i < argc; i++){
+        FILE *file_ptr; 
+        file_ptr = fopen(argv[i], "r");
 
-        printf("Check to see if file opened\n");
-        // check if the file was opened
-        if(file_ptr == NULL){               //If the file didn't open, error out and exit
-            perror("my-uniq: cannot open file\n");
-            exit(1); 
-        }
+        // get a line
+        char *line1 = NULL;
+        char *line2 = NULL;
+        size_t len1 = 0;
+        size_t len2 = 0;
+        size_t read1;
+        size_t read2;
 
-
-        //Go through each line
-        printf("start while loop\n");
-        while ((getline(&line, &length, file_ptr)) != EOF){
-
-            printf("Check to see if we allocated memory\n");
-            if (!line){
+        while ((read1 = getline(&line1, &len1, file_ptr)) != -1){
+            if (!line1){
                 // memory allocation failed
                 fclose(file_ptr);
-                perror("Memory allocation failed\n");
+                perror("Memory allocation failed");
                 exit(1);
             }
+            if ((read2 = getline(&line2, &len2, file_ptr)) == -1){
+                printf("%s", line1);
+                break;
+            }
+
+            if (strcmp(line1, line2) != 0){ // lines are different
+                printf("%s", line1);
+            }
             
-            printf("check to see if we arent the same as the last one\n");
-            //Reset out ignore flag if the last one didn't match
-            if (strcmp(line, lastline) != 0 ){
-                ignore = 0;
-            }
-
-            printf("Check to see if we should print\n");
-            //Check to see if we matched the last line and we haven't printed already
-            if ((strcmp(line, lastline ) == 0) && (ignore == 0)){
-                ignore = 1;             //Set our flag so we don't print again
-                printf("%s", line);     //print
-            }
-
-            //Copy the current line into last line for the next loop
-            strcpy(lastline, line);
-
         }
+        free(line1);
+        free(line2);
 
-
+        fclose(file_ptr);
     }
-    printf("End\n");
+
+
+
+    
+    // for (int i = 1; i < argc; i++){         //Go through each argument, which all should be files
+
+    //     printf("Initialize Variables\n");   
+    //     FILE *file_ptr;                     //Initialize variable to hold file
+    //     char* line = "";                  //Initialize variable to hold read line
+    //     char* lastline = "";              //Initialize variable to hold last line
+    //     size_t length = 0;                  //Initialize variable to hold the length
+    //     int ignore = 0;                     //Flag to only print once
+        
+    //     printf("Try and open file\n");
+    //     file_ptr = fopen(argv[i], "r");  //Attempt to open file given by user
+
+    //     printf("Check to see if file opened\n");
+    //     // check if the file was opened
+    //     if(file_ptr == NULL){               //If the file didn't open, error out and exit
+    //         perror("my-uniq: cannot open file\n");
+    //         exit(1); 
+    //     }
+
+
+    //     //Go through each line
+    //     printf("start while loop\n");
+    //     while ((getline(&line, &length, file_ptr)) != EOF){
+
+    //         printf("Check to see if we allocated memory\n");
+    //         if (!line){
+    //             // memory allocation failed
+    //             fclose(file_ptr);
+    //             perror("Memory allocation failed\n");
+    //             exit(1);
+    //         }
+            
+    //         printf("check to see if we arent the same as the last one\n");
+    //         //Reset out ignore flag if the last one didn't match
+    //         if (strcmp(line, lastline) != 0 ){
+    //             ignore = 0;
+    //         }
+
+    //         printf("Check to see if we should print\n");
+    //         //Check to see if we matched the last line and we haven't printed already
+    //         if ((strcmp(line, lastline ) == 0) && (ignore == 0)){
+    //             ignore = 1;             //Set our flag so we don't print again
+    //             printf("%s", line);     //print
+    //         }
+
+    //         //Copy the current line into last line for the next loop
+    //         strcpy(lastline, line);
+
+    //     }
+
+
+    // }
+    // printf("End\n");
 
 
     
